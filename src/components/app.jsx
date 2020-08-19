e614127855b505f1da65f9a61b9419bc99893790
Gavin Garza <48096041+RenGVZ@users.noreply.github.com>
2020-08-19 17:35:11 +0900

    finished react project

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -2,23 +2,40 @@ import React, { Component } from 'react';
 import SearchBar from './search_bar';
 import Gif from './gif';
 import GifList from './gif_list';
+import giphy from 'giphy-api';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      gifs: [],
+      selectedGifId: "fqtxCwUd2t6dCzILMq"
+    };
+  }
+
+  search = (query) => {
+    giphy('RWKxeBAYUzwNsEWxmRDRr8plikoy1sJm').search({
+      q: query,
+      rating: 'g',
+      limit: 15
+    }, (err, res) => {
+      this.setState({
+        gifs: res.data
+      });
+    });
+  }
   render() {
-    const gifs = [
-      { id: "fqtxCwUd2t6dCzILMq"},
-      { id: "31wVvW0sOur7O"},
-    ];
     return (
       <div>
         <div className="left-scene">
-          <SearchBar />
+          <SearchBar searchFunction={this.search} />
           <div className="selected-gif">
-            <Gif id="fqtxCwUd2t6dCzILMq" />
+            <Gif id={this.state.selectedGifId} />
           </div>
         </div>
         <div className="right-scene">
-          <GifList />
+          <GifList gifs={this.state.gifs} />
         </div>
       </div>
     );
--- src/index.jsx
+++ src/index.jsx
@@ -10,4 +10,3 @@ if (root) {
     <App />, root
   );
 }
- 
