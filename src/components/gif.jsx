14d8d1eb57f60af616b9822c0d7ebf9bbd821d8a
Gavin Garza <48096041+RenGVZ@users.noreply.github.com>
2020-08-19 17:41:15 +0900

    test

--- src/components/gif.jsx
+++ src/components/gif.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-class Gif extends Component {
+class Gif extends Component{
   render() {
     const src = `https://i.giphy.com/media/${this.props.id}/giphy.gif`;
     return (
